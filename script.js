b1e045851cf1c01035d9dd811f7c656ee71ec8b1
Artistry449 <artistry449@gmail.com>
2024-01-12 23:39:41 +0800

    added edit button functionality

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+"use strict"
 
-document.querySelector('.btnSubmit').addEventListener("click", function (e) {
+const submitButton = document.querySelector('.btnSubmit');
+
+submitButton.addEventListener("click", function (e) {
     const inputTitle = document.querySelector('.inputTitle');
     const inputDescription = document.querySelector('.inputDescription');
 
@@ -11,45 +14,75 @@ document.querySelector('.btnSubmit').addEventListener("click", function (e) {
     let tempTitle = title;
     let tempDescription = description;
 
-    if (title !== "" && description !== "") {
+    // if (title !== "" && description !== "") {
 
-        const list = document.querySelector('.list');
+    const list = document.querySelector('.list');
 
-        // creating container
-        const container = document.createElement("div");
-        container.classList.add('container');
+    // creating container
+    const container = document.createElement("div");
+    container.classList.add('container');
 
-        // creating to-do-item
-        const toDoItem = document.createElement("div");
-        toDoItem.innerHTML = `
+    // creating to-do-item
+    const toDoItem = document.createElement("div");
+    toDoItem.innerHTML = `
         <h4> ${title} </h4><p> ${description} </p>
         `;
 
-        // creating buttons
-        const editButton = document.createElement("button");
-        const deleteButton = document.createElement("button");
-        editButton.classList.add('editBtn');
-        deleteButton.classList.add('deleteBtn');
+    // creating buttons
+    const editButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+    editButton.classList.add('editBtn');
+    deleteButton.classList.add('deleteBtn');
 
-        // connecting toDoItem, editBtn, deleteBtn to container
-        container.appendChild(toDoItem);
-        container.appendChild(editButton);
-        container.appendChild(deleteButton);
+    // connecting toDoItem, editBtn, deleteBtn to container
+    container.appendChild(toDoItem);
+    container.appendChild(editButton);
+    container.appendChild(deleteButton);
 
-        list.appendChild(container);
+    list.appendChild(container);
 
-        toDoItem.classList.add('toDoItem');
+    toDoItem.classList.add('toDoItem');
 
 
-        deleteButton.addEventListener('click', function () {
-            const toDoContainer = this.closest('.container');
+    deleteButton.addEventListener('click', function () {
+        const toDoContainer = this.closest('.container');
 
-            if (toDoContainer) {
-                toDoContainer.remove();
-            }
-        })
+        if (toDoContainer) {
+            toDoContainer.remove();
+        }
+    });
 
-        inputTitle.value = "";
-        inputDescription.value = "";
+
+    editButton.addEventListener('click', editButtonClickHandler);
+
+    function editButtonClickHandler() {
+        editButton.classList.add('confirmBtn');
+
+        const containerTitle = container.querySelector('h4');
+        const containerDescription = container.querySelector('p');
+
+        inputTitle.value = containerTitle.textContent;
+        inputDescription.value = containerDescription.textContent;
+
+        editButton.removeEventListener('click', editButtonClickHandler);
+        editButton.addEventListener('click', confirmButtonClickHandler, { once: true });
+
+        function confirmButtonClickHandler() {
+            containerTitle.textContent = inputTitle.value;
+            containerDescription.textContent = inputDescription.value;
+
+            editButton.classList.remove('confirmBtn');
+            editButton.addEventListener('click', editButtonClickHandler, { once: true });
+
+            inputTitle.value = "";
+            inputDescription.value = "";
+        }
+
+        console.log(containerTitle.textContent + ' ' + containerDescription.textContent)
     }
+
+    inputTitle.value = "";
+    inputDescription.value = "";
+
+    // }
 });
