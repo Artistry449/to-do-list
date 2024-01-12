02740b82eecc357947826a392c94e7a32d592b19
Artistry449 <artistry449@gmail.com>
2024-01-13 00:30:36 +0800

    fixed to-dos' visibility

--- script.js
+++ script.js
@@ -11,76 +11,95 @@ submitButton.addEventListener("click", function (e) {
     const title = inputTitle.value;
     const description = inputDescription.value;
 
-    let tempTitle = title;
-    let tempDescription = description;
+    if (title !== "" && description !== "") {
 
-    // if (title !== "" && description !== "") {
+        const list = document.querySelector('.list');
 
-    const list = document.querySelector('.list');
+        // creating container
+        const container = document.createElement("div");
+        container.classList.add('container');
 
-    // creating container
-    const container = document.createElement("div");
-    container.classList.add('container');
-
-    // creating to-do-item
-    const toDoItem = document.createElement("div");
-    toDoItem.innerHTML = `
+        // creating to-do-item
+        const toDoItem = document.createElement("div");
+        toDoItem.innerHTML = `
         <h4> ${title} </h4><p> ${description} </p>
         `;
 
-    // creating buttons
-    const editButton = document.createElement("button");
-    const deleteButton = document.createElement("button");
-    editButton.classList.add('editBtn');
-    deleteButton.classList.add('deleteBtn');
+        // creating buttons
+        const editButton = document.createElement("button");
+        const deleteButton = document.createElement("button");
+        editButton.classList.add('editBtn');
+        deleteButton.classList.add('deleteBtn');
 
-    // connecting toDoItem, editBtn, deleteBtn to container
-    container.appendChild(toDoItem);
-    container.appendChild(editButton);
-    container.appendChild(deleteButton);
+        // connecting toDoItem, editBtn, deleteBtn to container
+        container.appendChild(toDoItem);
+        container.appendChild(editButton);
+        container.appendChild(deleteButton);
 
-    list.appendChild(container);
+        list.appendChild(container);
 
-    toDoItem.classList.add('toDoItem');
+        toDoItem.classList.add('toDoItem');
 
 
-    deleteButton.addEventListener('click', function () {
-        const toDoContainer = this.closest('.container');
+        deleteButton.addEventListener('click', function () {
+            const toDoContainer = this.closest('.container');
 
-        if (toDoContainer) {
-            toDoContainer.remove();
+            if (toDoContainer) {
+                toDoContainer.remove();
+            }
+        });
+
+
+        editButton.addEventListener('click', editButtonClickHandler);
+
+        function editButtonClickHandler() {
+            editButton.classList.add('confirmBtn');
+            submitButton.classList.add('inActive');
+
+            editButton.classList.add('active');
+
+            const containerTitle = container.querySelector('h4');
+            const containerDescription = container.querySelector('p');
+
+            inputTitle.value = containerTitle.textContent;
+            inputDescription.value = containerDescription.textContent;
+
+            editButton.removeEventListener('click', editButtonClickHandler);
+            editButton.addEventListener('click', confirmButtonClickHandler, { once: true });
+
+            const allEditBtns = document.querySelectorAll(".editBtn");
+
+            console.log(allEditBtns);
+
+            for (let i = 0; i < allEditBtns.length; i++) {
+                if (!allEditBtns[i].classList.contains('active')) {
+                    allEditBtns[i].classList.add('inActive');
+                }
+            }
+
+            function confirmButtonClickHandler() {
+
+                for (let i = 0; i < document.querySelectorAll(".editBtn").length; i++) {
+                    if (!allEditBtns[i].classList.contains('active')) {
+                        allEditBtns[i].classList.remove('inActive');
+                    }
+                }
+
+                submitButton.classList.remove('inActive');
+                containerTitle.textContent = inputTitle.value;
+                containerDescription.textContent = inputDescription.value;
+
+                editButton.classList.remove('confirmBtn');
+                editButton.classList.remove('active');
+                editButton.addEventListener('click', editButtonClickHandler, { once: true });
+
+                inputTitle.value = "";
+                inputDescription.value = "";
+            }
         }
-    });
 
+        inputTitle.value = "";
+        inputDescription.value = "";
 
-    editButton.addEventListener('click', editButtonClickHandler);
-
-    function editButtonClickHandler() {
-        editButton.classList.add('confirmBtn');
-
-        const containerTitle = container.querySelector('h4');
-        const containerDescription = container.querySelector('p');
-
-        inputTitle.value = containerTitle.textContent;
-        inputDescription.value = containerDescription.textContent;
-
-        editButton.removeEventListener('click', editButtonClickHandler);
-        editButton.addEventListener('click', confirmButtonClickHandler, { once: true });
-
-        function confirmButtonClickHandler() {
-            containerTitle.textContent = inputTitle.value;
-            containerDescription.textContent = inputDescription.value;
-
-            editButton.classList.remove('confirmBtn');
-            editButton.addEventListener('click', editButtonClickHandler, { once: true });
-
-            inputTitle.value = "";
-            inputDescription.value = "";
-        }
     }
-
-    inputTitle.value = "";
-    inputDescription.value = "";
-
-    // }
 });
